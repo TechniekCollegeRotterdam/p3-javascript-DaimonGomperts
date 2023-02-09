c88c9f4cdbf5da337c136fc9d36d59abb5785052
DaimonGomperts <9018480@stundent.zadkine.nl>
2023-02-09 13:23:08 +0100

    2.4

---
+++ week2/2.4/app.js
@@ -0,0 +1,26 @@
+
+//opdracht 1
+function calculator(num, num2){
+     let answer = num + num2
+      console.log(answer)
+ }
+
+calculator(9, 13);
+
+//opdracht 2
+
+function multiplybifive(num, num2){
+let answer = num * 5
+console.log(answer)
+}
+
+multiplybifive(4)
+
+//opdracht 3
+ 
+function calculateMinutes(num, num2){
+    let answer = num / 60
+    console.log(answer)
+}
+
+calculateMinutes(3600)
---
+++ week2/app.js
@@ -0,0 +1,33 @@
+// //haal het element "title" op uit mijn html en geef het een styling
+// //document zoekt in je html
+// let title = document.getElementById("title");
+// //maak een variabel image met je element image
+//  let image = document.getElementById("image");
+// //maak een button met je element button
+// let button = document.getElementById("button");
+
+// //maak een function genaamd removeImage 
+// function removeImage(){
+//     title.style.color="blue";
+//     image.style.display="none";
+// } 
+
+
+// //oproepen van een fucntie
+// //removeImage();
+
+// function showImage(){
+//     title.style.color = "black"
+//     image.style.display="block"
+// }
+
+// //controleren of je de button element hebt opgehaald
+// //console.log(document.getElementById("button"));
+
+function calc(num, num2){
+ let answer = num - num2
+console.log(answer)
+
+}
+
+calc(7, 8);
