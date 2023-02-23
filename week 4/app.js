ef4ddeb04e9cd69d8b93a1b767ce3e094b460594
DaimonGomperts <9018480@stundent.zadkine.nl>
2023-02-23 11:37:31 +0100

    4.2

--- week 4/app.js
+++ week 4/app.js
@@ -3,17 +3,28 @@ let input1 = document.getElementById("num1");
 let input2 = document.getElementById("num2");
 
 function multiply() {
+
     answer.innerText = input1.value * input2.value
+      if(input1.value <= 0 || input2.value <= 0) 
+           answer.innerText = "Het antwoord is te laag";
+
 }
 
 function divide() {
     answer.innerText = input1.value / input2.value
+      if(input1.value <= 0 || input2.value <= 0)
+        answer.innerText = "Het antwoord is te laag";
 }
 
 function add ( ) {
     answer.innerText = Number(input1.value) + Number(input2.value)
+      if(input1.value <= 0 || input2.value <= 0)
+        answer.innerText = "Het antwoord is te laag";
 }
 
 function minus() {
     answer.innerText = input1.value - input2.value
+      if(input1.value <= 0 || input2.value <= 0)
+        answer.innerText = "Het antwoord is te laag";
 }
+
